254e6240510c12e8e0101f01e41dc49b926f838a
szebestik@gmail.com <szebestik@gmail.com>
2021-07-15 09:41:18 +0200

    fixed the z-index of the navbar, clicking on the logo now moves the main page to the top

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,15 @@ import image from '../../assets/covid-19.png'
 import classes from './Navbar.module.css'
 
 function Navbar() {
+
+    function scrollToTop() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
     return (
         <nav className={classes.navbar}>
             <Link to='/'>
-                <img src={image} alt="logo" width="120px" height="60px"/>
+                <img src={image} alt="logo" width="120px" height="60px" onClick={scrollToTop}/>
             </Link>
             <h1>Covid-19 stats</h1>
         </nav>
